3700cb7a0d9359bfe65e91cacf2f4fc1d831775d
INDEZENDE <i.rios.zendejas@gmail.com>
2018-02-14 23:47:28 -0600

    1012

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Agregando el comentario 1010
+// Agregando el 1012
 function readFile() {
   if (this.files && this.files[0]) {
     var newImages = new FileReader();
@@ -11,7 +11,7 @@ function paintImage(e) {
   realTimeImage.src = e.target.result;
   // console.log(e.target);
 }
-// Agregando comentario 1011
+
 // Obteniendo los elemtos
 var realTimeComment = document.getElementById("real-time-comment");
 var area = document.getElementById("text-comment");
@@ -28,3 +28,51 @@ sendComment.addEventListener("click", printComments);
 textColor.addEventListener("click", changeTextColor);
 backgroundColor.addEventListener("click", chnageBackgroundColor);
 selectImage.addEventListener("change", readFile);
+// ciclos para recorrer las clases de los botones
+for (var i = 0; i < sizeText.length; i++) {
+  sizeText[i].addEventListener("click", changeSizeText);
+}
+for (var i = 0; i < alignText.length; i++) {
+  alignText[i].addEventListener("click", changeAlignText);
+}
+// para que se vea en tiempo real
+function getTextPreview() {
+  var enteredText = area.value;
+  realTimeComment.innerText = enteredText;
+  return realTimeComment;
+}
+// Para cambiar los tamaños de los comentarios
+function changeSizeText() {
+  if (this.value === "large") {
+    realTimeComment.style.fontSize = "50px";
+  } else if (this.value === "medium") {
+    realTimeComment.style.fontSize = "40px";
+  } else {
+    realTimeComment.style.fontSize = "20px";
+  }
+}
+// Para cambiar  la alineacion del texto
+function changeAlignText (){
+  realTimeComment.style.textAlign = this.value;
+}
+// Para cambiar el color del texto
+function changeTextColor (){
+var textColor = prompt("Elige el color del texto");
+realTimeComment.style.color = textColor;
+}
+// Cambiar el color de fondo del texto
+function chnageBackgroundColor (){
+  var backgroundText = prompt("Elige el fondo de tu texto");
+  realTimeComment.style.backgroundColor = backgroundText;
+}
+// pintar comentio en publicaciones
+function printComments (){
+  var text = getTextPreview();
+  var publishComment = text.parentNode.cloneNode(true);
+  console.log(text.parentNode);
+  commentedPost.insertBefore(publishComment, commentedPost.firstChild);
+  area.value = " ";
+  text.removeAttribute("style");
+  text.innerText = " ";
+  // document.getElementById("real-time-image").removeAttribute("src");
+}
